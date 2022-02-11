59cb3c1962e70b15dc9ee89e865c5a044d31f451
jinal2511 <jinal_patel@codenticsoftware.com>
2022-02-11 10:40:36 +0530

    create header folder

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
- import Header from "./header";
- import Link from "next/link";
-import Mittal from "./new";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Header from "./headres/header";
 export default function Home() {
-
-  // const [main, setMain] = useState(false);
   return (
-
     <>
       <div>
         <div className="container">
@@ -14,24 +9,8 @@ export default function Home() {
             <img src="/logo.svg" />
           </div>
           <Header />
-   
-          {/* <div className="mobileview">
-            <Link href="/Girls">
-              <a className="girlfont">GIRLS</a>
-            </Link>
-            <Link href="/Boys">
-              <a className="boyfont">BOYS</a>
-            </Link>
-            <Link href="/Everyone">
-              <a className="everyfont">EVERYONE</a>
-            </Link>
-            <Link href="/Accessories">
-              <a className="accefont">ACCESSORIES </a>
-            </Link>
-          </div> */}
         </div>
-        
       </div>
     </>
- );
+  );
 }
