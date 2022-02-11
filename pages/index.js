3ea8fc114c2110c4884c2338c97d61fa65d95e78
jinal2511 <jinal_patel@codenticsoftware.com>
2022-02-11 18:47:27 +0530

    add img banner

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
-import Header from "./headres/Header";
+import Header from "./headres/header";
+
 export default function Home() {
   return (
-    <>
+  
       <div>
         <div className="container">
           <div className="logoimg">
             <img src="/logo.svg" />
           </div>
           <Header />
-        </div>
+          </div>
+        <div>
+          <div>
+            <img src="/bannerImg.svg" className="bannerImg"/>
+          </div>
+          </div>
       </div>
-    </>
+   
   );
 }
